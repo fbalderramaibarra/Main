7fde19ad9694af57e67e097eb16889f47aa7dafb
Fernando Balderrama Ibarra <fbalderrama.ibarra@gmail.com>
2021-10-04 21:35:43 -0400

    Update ScannerLearn.java

--- ScannerLearn.java
+++ ScannerLearn.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class ScannerLearn {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String line;
 		Scanner in = new Scanner(System.in);
 		System.out.print("Type something: ");
